0f891e45ef3bc4bda1a66b07c92b02c6124f5113
sirjava <sirjava@0c6d3e56-2eb1-11de-8ac3-93d7c2238582>
2009-11-17 19:19:33 +0000

    support for the new json syntax of .jsb2 files.

--- trunk/testRun.pl
+++ trunk/testRun.pl
@@ -16,8 +16,8 @@ use JsbDepRes;
 # file locations
 # paths generated in the .jsb are relative to $jsbRoot
 # the .jsb file should therefore always live in the jsbRoot so it references paths in the .jsb correctly
-my $jsbRoot = "/cygdrive/c/apps/eclipse_workspace/jsb-builder/js/";
-my $jsbFile = "$jsbRoot/test.jsb";
+my $jsbRoot = "/cygdrive/c/PROGRAMMER/eclipse-workspace_3.5/jsb-builder/js/";
+my $jsbFile = $jsbRoot ."test.jsb2";
 
 # rebuild the $jsbFile
 print "Calculating Dependencies...\n";
@@ -25,14 +25,21 @@ my $j = new JsbDepRes($jsbRoot);
 my @deps = $j->getDeps();
 
 open(JSB, ">$jsbFile"); # open jsb file for overwrite
-print "Writing .jsb:\n";
-print JSB "<?xml version='1.0' encoding='utf-8'?><project path='' name='jsb-builder-test'><target name='Everything'>";
+print "Writing .jsb2:\n";
+print JSB "{\n  projectName: 'jsb2 includes', \n  pkgs: [{\n    file: '$jsbFile',\n    name: 'Everything', \n    fileIncludes: [{\n    ";
 
+my @lines = ();
+my $file = '';
+my $dir = '';
 for my $dep(@deps) {
-    $dep =~ s/\//\\/g; # replace / with \ for windows 
-	print JSB "\n<include name='$dep'/>";
+#    $dep =~ s/\//\\/g; # replace / with \ for windows 
+    # split path & file, win & linux compatable
+    $dep =~ m/((.*)[\\\/])?(.+)/;
+    ($dir,$file) =  ( $1 || '', $3 || '' );
+    push(@lines, "  text: '$file',\n        path: '$dir'");
 }
-print JSB "\n</target></project>";
+my $data = join("\n      },{\n      ",@lines);
+print JSB "$data\n    }]\n  }]\n}";
 close(JSB);
 
 print "Done.\n";
